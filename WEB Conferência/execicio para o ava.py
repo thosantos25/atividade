4db75e29cfcdb9b60d9a785b4373f3b0439a3dd5
thosantos25 <thalissonsantos569@gmail.com>
2024-10-02 00:31:34 +0000

    exerciccio ava

---
+++ WEB Conferência/execicio para o ava.py
@@ -0,0 +1,20 @@
+# Limpa o Terminal.
+import os
+
+os.system("cls || clear")
+
+# Variaveis.
+user = "Usuário 1º"
+user = "Usuário 2º"
+user = "Usuário 3º"
+user = "Usuário 4º"
+
+# Função for.
+
+for i in range(1):
+    print("Usuário 1º")
+    print("Usuário 2º")
+    print("Uduário 3º")
+    print("Usuário 4º")
+
+print("=== FIM DO PROGRAMA ===")    
