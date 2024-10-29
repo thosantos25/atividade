59b5b8f417fed71c55a781c263762f5250f66efc
thosantos25 <thalissonsantos569@gmail.com>
2024-10-29 23:12:17 +0000

    situção

--- Aulas 28/Situação de aprendizagem/Situação de Aprendizagem 2 – Etapa 02.py
+++ Aulas 28/Situação de aprendizagem/Situação de Aprendizagem 2 – Etapa 02.py
@@ -3,13 +3,22 @@ import os
 os.system("cls || clear") #Limpa o terminal (|| significa ou).
 
 #Entrado.
-nome = input("Digite seu nome: ")
-idade = int(input("Digite sua idade:")) 
-telefone = int(input("Digite seu peso:"))
+nome = input("Digite seu Nome: ")
+idade = int(input("Digite sua Idade:")) 
+telefone = int(input("Digite seu Telefone:"))
+cpf = input("Digite seu CPF: ")
+rg = input("Digite seu RG: ")
+endereço = input("Digite seu Endereço: ")
+sexo = input("Digite seu Sexo(a): ")
 
 #Saída.
 print(f"Nome: ", nome)
 print(f"Idade: {idade}")
+print(f"Telefone: {telefone}")
+print(f"CPF: {cpf}")
+print(f"RG: {rg}")
+print(f"Endereço: {endereço}")
+print(f"Sexo: {sexo}")
 
 
 
