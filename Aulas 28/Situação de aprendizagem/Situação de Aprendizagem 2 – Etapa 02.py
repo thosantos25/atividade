c7873ae99c77cc88f46099d06b17a889246dde74
thosantos25 <thalissonsantos569@gmail.com>
2024-10-29 22:43:03 +0000

    j

---
+++ Aulas 28/Situação de aprendizagem/Situação de Aprendizagem 2 – Etapa 02.py
@@ -0,0 +1,15 @@
+import os
+
+os.system("cls || clear") #Limpa o terminal (|| significa ou).
+
+#Entrado.
+nome = input("Digite seu nome: ")
+idade = int(input("Digite sua idade:")) 
+telefone = int(input("Digite seu peso:"))
+
+#Saída.
+print(f"Nome: ", nome)
+print(f"Idade: {idade}")
+
+
+
