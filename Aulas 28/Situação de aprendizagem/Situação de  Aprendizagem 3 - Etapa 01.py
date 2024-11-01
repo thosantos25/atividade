fb2efffd4419501daa4ff3e062a84dd9f2070a0b
thosantos25 <thalissonsantos569@gmail.com>
2024-11-01 00:39:28 +0000

    salvar documento

---
+++ Aulas 28/Situação de aprendizagem/Situação de  Aprendizagem 3 - Etapa 01.py
@@ -0,0 +1,18 @@
+# Limpa o Terminal.
+import os
+
+os.system(" cls || clear")
+
+# Entrada de Dados.
+
+int(input("Digite quantidade de usuários que deseja cadastrar: \n"))
+print("\n")
+
+# Menu de cadastro.
+
+print("""\nMenu de Opções
+      1 - Cadastrar Novo Usuário
+      2 - Listar todos os usuários cadastrados
+      3 - Sair do sistema""")
+
+quantidadedeusuarios = []
