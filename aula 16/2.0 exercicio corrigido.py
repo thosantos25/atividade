a7734c9eb465bf5837b51563981734090527ce3f
thosantos25 <thalissonsantos569@gmail.com>
2024-11-16 15:31:37 +0000

    Mesagem

--- aula 16/2.0 exercicio corrigido.py
+++ aula 16/2.0 exercicio corrigido.py
@@ -2,21 +2,37 @@ import os
 os.system("cls || clear")
 # limpa o terminal
 
-# Fefatore este código para uso com uma função.
+"""Refatore: 
+    1 - Crie um função para verificar se está aprovado.
+    - Conidere média maior ou igual a 7,0 para aprovação
+    - caso contrário, reprovado
+        
+    2 - Use um lista para armazenar notas.
+    """
+
 # Inicializando variáveis.
-notas = 0 
+notas = [] # Lista 
 QUANTIDADE_NOTAS = 4
 
 def calcular_media(notas, quantidade_notas):
-        media = notas / quantidade_notas
+        media = sum(notas) / quantidade_notas
         return media
+
+def verificar_aprovacao(media):
+        if QUANTIDADE_NOTAS >= 7:
+                resultado = "Reprovado"
+        else:
+                resultado = "Aprovado"
+        return resultado
 
 # ENTRADA
 for i in range(QUANTIDADE_NOTAS):
-        notas += float(input("Digite uma nota: "))
-
+        nota = float(input("Digite uma nota: "))
+        notas.append(nota)
 # Processamento
 media = calcular_media(notas, QUANTIDADE_NOTAS)
+resultado = verificar_aprovacao(media)
 
 # Saída 
-print(f"Média: {media}")
+print(f"\nMédia: {media}")
+print(f"Resultado: {resultado}")
