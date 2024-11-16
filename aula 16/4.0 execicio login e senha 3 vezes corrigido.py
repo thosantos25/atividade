a4fe1ff179a515b7b7f575bbb48388e6a6ffc282
thosantos25 <thalissonsantos569@gmail.com>
2024-11-16 16:05:55 +0000

    Login e senha 3 tentativas

--- aula 16/4.0 execicio login e senha 3 vezes corrigido.py
+++ aula 16/4.0 execicio login e senha 3 vezes corrigido.py
@@ -6,17 +6,23 @@ login_cadastrado = "marta"
 senha_cadastra = "123"
 tentativas = 0
 
+def verificar_login_e_senha(login, senha, tentativas):
+    if login == login_cadastrado and senha == senha_cadastra:
+           resultado = True
+    else:
+          resultado = False
+    return resultado
+
+
 # Entrada.
 while True:
-    login = input("Digite seu login: ")
-    senha = input("Digite sua senha: ")
-    tentativas += 1
-    
-    # Processamento
-    if login == login_cadastrado and senha_cadastra:
-        print("Acesso ao sistema!")
-        break
-    
-    if tentativas >= 3:
-        print("Permitido  anpenas 3 tentativas!")
-        break
+        login = input("Digite seu login: ")
+        senha = input("Digite sua senha: ")
+        tentativas += 1   
+        # Processamento
+        if verificar_login_e_senha(login, senha, tentativas):
+              print("Acesso ao sistem!")
+              break
+        if tentativas >= 3:
+              print("Permitido apenas 3 tentativas!")
+              break
