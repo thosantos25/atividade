e081eeeb1f4d67cea99618519cc8538baa9f7062
thosantos25 <thalissonsantos569@gmail.com>
2024-11-16 14:57:58 +0000

    Def nota

--- aula 16/2.0 exercicio corrigido.py
+++ aula 16/2.0 exercicio corrigido.py
@@ -2,15 +2,21 @@ import os
 os.system("cls || clear")
 # limpa o terminal
 
+# Fefatore este código para uso com uma função.
 # Inicializando variáveis.
 notas = 0 
 QUANTIDADE_NOTAS = 4
+
+def calcular_media(notas, quantidade_notas):
+        media = notas / quantidade_notas
+        return media
+
 # ENTRADA
 for i in range(QUANTIDADE_NOTAS):
-    notas += float(input("Digite uma nota: "))
+        notas += float(input("Digite uma nota: "))
 
 # Processamento
-media = notas / QUANTIDADE_NOTAS
+media = calcular_media(notas, QUANTIDADE_NOTAS)
 
 # Saída 
 print(f"Média: {media}")
---
+++ aula 16/5.0 refatoramento.py
@@ -0,0 +1,16 @@
+import os 
+os.system("cls || clear")
+
+# Refatore esta código para uso com uma função.
+
+# Funçoes 
+
+def pares_impares():
+    for i in range(1, 16, 2):
+        if i % 2 == 1:
+            print(i)
+
+# Chamada da função.
+pares_impares()
+
+
